a33d470e79037624aad0e270286c865ed26f3e78
Richard Forrest <richard.a.forrest@gmail.com>
2019-08-07 12:07:04 +0100

    Basic logging serilog

---
+++ src/QandA/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Serilog;
+using Serilog.Events;
+
+namespace QandA
+{
+    public class Program
+    {
+		public static int Main(string[] args)
+		{
+			Log.Logger = new LoggerConfiguration()
+				.MinimumLevel.Debug()
+				.MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+				.Enrich.FromLogContext()
+				.WriteTo.Console()
+				.WriteTo.Seq("http://localhost:5341")
+				.CreateLogger();
+
+			try
+			{
+				Log.Information("Starting web host");
+				CreateWebHostBuilder(args).Build().Run();
+				return 0;
+			}
+			catch (Exception ex)
+			{
+				Log.Fatal(ex, "Host terminated unexpectedly");
+				return 1;
+			}
+			finally
+			{
+				Log.CloseAndFlush();
+			}
+		}
+
+		public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+			WebHost.CreateDefaultBuilder(args)
+				.UseStartup<Startup>()
+				.UseSerilog();
+	}
+}
