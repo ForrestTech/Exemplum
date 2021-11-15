16302426401b3be325d64a4a77c08d9ff8643379
richard.a.forrest@gmail.com <richard.a.forrest@gmail.com>
2021-11-15 10:48:06 +0700

    Dotnet 6 unit tests

--- tests/LoadTests/Program.cs
+++ tests/LoadTests/Program.cs
@@ -1,39 +1,29 @@
-﻿namespace Exemplum.LoadTests
-{
-    using NBomber.CSharp;
-    using NBomber.Plugins.Http.CSharp;
-    using NBomber.Plugins.Network.Ping;
-    using System;
+﻿using NBomber.CSharp;
+using NBomber.Plugins.Http.CSharp;
+using NBomber.Plugins.Network.Ping;
 
-    class Program
+var step = Step.Create("get_task_list",
+    clientFactory: HttpClientFactory.Create(),
+    execute: context =>
     {
-        protected static void Main(string[] args)
-        {
-            var step = Step.Create("get_task_list",
-                clientFactory: HttpClientFactory.Create(),
-                execute: context =>
-                {
-                    var request = Http.CreateRequest("GET", "https://localhost:5001/api/todolist")
-                        .WithHeader("Accept", "application/json");
+        var request = Http.CreateRequest("GET", "https://localhost:5001/api/todolist")
+            .WithHeader("Accept", "application/json");
 
-                    return Http.Send(request, context);
-                });
+        return Http.Send(request, context);
+    });
 
-            var scenario = ScenarioBuilder
-                .CreateScenario("task_list", step)
-                .WithWarmUpDuration(TimeSpan.FromSeconds(5))
-                .WithLoadSimulations(
-                    Simulation.InjectPerSec(rate: 50, during: TimeSpan.FromSeconds(10))
-                );
+var scenario = ScenarioBuilder
+    .CreateScenario("task_list", step)
+    .WithWarmUpDuration(TimeSpan.FromSeconds(5))
+    .WithLoadSimulations(
+        Simulation.InjectPerSec(rate: 50, during: TimeSpan.FromSeconds(10))
+    );
 
-            // creates ping plugin that brings additional reporting data
-            var pingPluginConfig = PingPluginConfig.CreateDefault(new[] {"https://localhost:5001/api/todolist"});
-            var pingPlugin = new PingPlugin(pingPluginConfig);
+// creates ping plugin that brings additional reporting data
+var pingPluginConfig = PingPluginConfig.CreateDefault(new[] {"https://localhost:5001/api/todolist"});
+var pingPlugin = new PingPlugin(pingPluginConfig);
 
-            NBomberRunner
-                .RegisterScenarios(scenario)
-                .WithWorkerPlugins(pingPlugin)
-                .Run();
-        }
-    }
-}
+NBomberRunner
+    .RegisterScenarios(scenario)
+    .WithWorkerPlugins(pingPlugin)
+    .Run();
