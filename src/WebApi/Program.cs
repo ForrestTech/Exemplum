9b3d26c212b7af44e84a0e6cb45e877d2eb869fc
richard.a.forrest@gmail.com <richard.a.forrest@gmail.com>
2021-11-18 09:26:41 +0700

    Clean up program to make it more minimal

--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -1,166 +1,126 @@
-try
+Activity.DefaultIdFormat = ActivityIdFormat.W3C;
+
+Log.Logger = LogConfiguration.CreateLogger();
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog();
+
+builder.Services.AddApplication(builder.Configuration, builder.Environment);
+builder.Services.AddInfrastructure(builder.Configuration, builder.Environment);
+
+builder.Services.AddCors(options =>
 {
-    Activity.DefaultIdFormat = ActivityIdFormat.W3C;
-
-    Log.Logger = LogConfiguration.CreateLogger();
-
-    var builder = WebApplication.CreateBuilder(args);
-
-    builder.Host.UseSerilog();
-
-    Log.Information("Starting API host");
-
-    builder.Services.AddApplication(builder.Configuration, builder.Environment);
-    builder.Services.AddInfrastructure(builder.Configuration, builder.Environment);
-
-    builder.Services.AddCors(options =>
+    options.AddPolicy("Default", policyOptions =>
     {
-        options.AddPolicy("Default", policyOptions =>
-        {
-            policyOptions
-                .WithOrigins(builder.Configuration.GetServiceUri("webapp")?.ToString() ?? "https://localhost:6001")
-                .AllowAnyOrigin()
-                .AllowAnyHeader()
-                .AllowAnyMethod();
-        });
+        policyOptions
+            .WithOrigins(builder.Configuration.GetServiceUri("webapp")?.ToString() ?? "https://localhost:6001")
+            .AllowAnyOrigin()
+            .AllowAnyHeader()
+            .AllowAnyMethod();
     });
+});
 
-    builder.Services.AddAuthentication(options =>
-    {
-        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-    }).AddJwtBearer(options =>
-    {
-        options.Authority = builder.Configuration["Auth0:Authority"];
-        options.Audience = builder.Configuration["Auth0:ApiIdentifier"];
-        options.TokenValidationParameters = new TokenValidationParameters {NameClaimType = "name", RoleClaimType = "https://schemas.dev-ememplum.com/roles"};
-    });
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.Authority = builder.Configuration["Auth0:Authority"];
+    options.Audience = builder.Configuration["Auth0:ApiIdentifier"];
+    options.TokenValidationParameters = new TokenValidationParameters {NameClaimType = "name", RoleClaimType = "https://schemas.dev-ememplum.com/roles"};
+});
 
-    var hcBuilder = builder.Services.AddHealthChecks()
-        .ForwardToPrometheus();
+var hcBuilder = builder.Services.AddHealthChecks()
+    .ForwardToPrometheus();
 
-    if (!builder.Configuration.UseInMemoryStorage())
-    {
-        hcBuilder.AddSqlServer(builder.Configuration.GetDefaultConnection());
-    }
+if (!builder.Configuration.UseInMemoryStorage())
+{
+    hcBuilder.AddSqlServer(builder.Configuration.GetDefaultConnection());
+}
 
-    //This does not seem to work with dotnet 6            
-    //builder.Services.AddHealthChecksUI()
-    //  .AddInMemoryStorage();
+//This does not seem to work with dotnet 6            
+//builder.Services.AddHealthChecksUI()
+//  .AddInMemoryStorage();
 
-    builder.Services.AddControllers().AddFluentValidation(x => x.AutomaticValidationEnabled = false);
+builder.Services.AddControllers().AddFluentValidation(x => x.AutomaticValidationEnabled = false);
 
-    builder.Services.AddHttpsRedirection(options =>
-    {
-        options.HttpsPort = 5001;
-    });
+builder.Services.AddHttpsRedirection(options =>
+{
+    options.HttpsPort = 5001;
+});
 
 builder.Services.Configure<ApiBehaviorOptions>(options =>
-    {
-        options.SuppressModelStateInvalidFilter = true;
-    });
-
-    builder.Services.Configure<RouteOptions>(options =>
-    {
-        options.LowercaseUrls = true;
-    });
-
-    builder.Services.AddEndpointsApiExplorer();
-    builder.Services.AddSwaggerGen(c =>
-    {
-        c.SwaggerDoc("v1", new OpenApiInfo {Title = "Exemplum", Version = "v1"});
-        var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-        var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-        
-        c.IncludeXmlComments(xmlPath);
-    });
-
-    var app = builder.Build();
-
-    if (app.Environment.IsDevelopment())
-    {
-        app.UseApiExceptionHandler(true);    
-    }
-    else
-    {
-        app.UseApiExceptionHandler(false);
-        app.UseHsts();
-    }
-
-    app.UseSerilogRequestLogging();
-    app.UseHttpsRedirection();
-
-    app.UseRouting();
-    app.UseHttpMetrics();
-
-    app.UseCors("Default");
-
-    app.UseAuthentication();
-    app.UseAuthorization();
-
-    app.UseMetricServer();
-
-    app.UseEndpoints(endpoints =>
-    {
-        //with a more complex set of health checks we can separate checks by tag
-        endpoints.MapHealthChecks("/health/ready",
-            new HealthCheckOptions {Predicate = (_) => true, ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse});
-        endpoints.MapHealthChecks("/health/live", new HealthCheckOptions {Predicate = (_) => false});
-
-        endpoints.MapHealthChecksUI();
-
-        endpoints.MapControllers();
-
-        endpoints.MapMetrics();
-    });
-
-    app.MapSwagger();
-    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Exemplum"));
-
-    //we are using both minimal API and Controllers as examples
-    app.MapWeatherForecastEndpoints();
-
-    await SeedDatabase(app);
-    
-    await app.RunAsync();
-
-    return 0;
-}
-catch (Exception ex)
 {
-    Log.Fatal(ex, "Host terminated unexpectedly");
-    return 1;
-}
-finally
+    options.SuppressModelStateInvalidFilter = true;
+});
+
+builder.Services.Configure<RouteOptions>(options =>
 {
-    Log.CloseAndFlush();
-}
+    options.LowercaseUrls = true;
+});
 
-
-static async Task SeedDatabase(IHost host)
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
 {
-    using var scope = host.Services.CreateScope();
+    c.SwaggerDoc("v1", new OpenApiInfo {Title = "Exemplum", Version = "v1"});
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
 
-    var serviceProvider = scope.ServiceProvider;
+    c.IncludeXmlComments(xmlPath);
+});
 
-    try
-    {
-        var context = serviceProvider.GetRequiredService<ApplicationDbContext>();
+var app = builder.Build();
 
-        if (context.Database.IsSqlServer())
-        {
-            await context.Database.MigrateAsync();
-        }
-
-        ApplicationDbContextSeed.SeedSampleDataAsync(context);
-    }
-    catch (Exception ex)
-    {
-        Log.Error(ex, "An error occurred while migrating or seeding the database");
-
-        throw;
-    }
+if (app.Environment.IsDevelopment())
+{
+    app.UseApiExceptionHandler(true);
 }
+else
+{
+    app.UseApiExceptionHandler(false);
+    app.UseHsts();
+}
+
+app.UseSerilogRequestLogging();
+app.UseHttpsRedirection();
+
+app.UseRouting();
+app.UseHttpMetrics();
+
+app.UseCors("Default");
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseMetricServer();
+
+app.UseEndpoints(endpoints =>
+{
+    //with a more complex set of health checks we can separate checks by tag
+    endpoints.MapHealthChecks("/health/ready",
+        new HealthCheckOptions {Predicate = (_) => true, ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse});
+    endpoints.MapHealthChecks("/health/live", new HealthCheckOptions {Predicate = (_) => false});
+
+    endpoints.MapHealthChecksUI();
+
+    endpoints.MapControllers();
+
+    endpoints.MapMetrics();
+});
+
+app.MapSwagger();
+app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Exemplum"));
+
+//we are using both minimal API and Controllers as examples
+app.MapWeatherForecastEndpoints();
+
+await Seeder.SeedDatabase(app);
+
+await app.RunAsync();
+
+return 0;
 
 // Make the implicit Program class public so test projects can access it
 public partial class Program
