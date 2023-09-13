935c1e04c9ee51388bbb2b55cbace3b2805db3e6
Richard Forrest <richard.a.forrest@gmail.com>
2023-09-13 10:42:37 +0100

    Migrate to postgresql
    Setup docker compose for postgresql, redis and rabbit use sqllite as
    default local db and only use in memory for unit tests
    
    resolves #160
    resolves #159
    resolves #158

---
+++ src/Infrastructure/Seeder.cs
@@ -0,0 +1,47 @@
+﻿namespace Exemplum.Infrastructure;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Persistence;
+
+public static class Seeder
+{
+    public static async Task SeedDatabase(IServiceProvider appServices)
+    {
+        using var scope = appServices.CreateScope();
+
+        var serviceProvider = scope.ServiceProvider;
+
+        try
+        {
+            
+            var logger = serviceProvider.GetRequiredService<Microsoft.Extensions.Logging.ILogger>();
+            var context = serviceProvider.GetRequiredService<ApplicationDbContext>();
+            
+            logger.LogInformation("Ensure database is created");
+            
+            if (context.Database.IsNpgsql())
+            {
+                logger.LogInformation("PostgresSql running migrations");
+                await context.Database.MigrateAsync();
+            }
+            else
+            {
+                logger.LogInformation("SqlLite Database running auto create");
+                await context.Database.EnsureCreatedAsync();
+            }
+
+            logger.LogInformation("Seeding data");
+            
+            ApplicationDbContextSeed.SeedDatabase(context);
+            
+            logger.LogInformation("Data seeded");
+        }
+        catch (Exception ex)
+        {
+            Log.Error(ex, "An error occurred while migrating or seeding the database");
+
+            throw;
+        }
+    }    
+}
